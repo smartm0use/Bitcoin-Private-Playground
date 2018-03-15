d132f5eb88b450186db213e623e09a51af6bb4be
smartmouse <smartmouse@gmail.com>
2018-03-15 12:24:59 +0100

    null data transaction

--- create-account.js
+++ create-account.js
@@ -3,18 +3,18 @@ const Btc = require('bitcoinjs-lib');
 const TestNet = Btc.networks.testnet;
 const apiUrl = 'https://testnet.blockexplorer.com/api/addr/';
 
-// let keyPair = Btc.ECPair.makeRandom({network: TestNet});
-// let publicKey = keyPair.getAddress();
-// let privateKey = keyPair.toWIF();
-// console.log(`Public: ${publicKey} \nPrivate: ${privateKey}`);
+var keyPair = Btc.ECPair.makeRandom({network: TestNet});
+var address = keyPair.getAddress();
+var privateKey = keyPair.toWIF();
+console.log(`Indirizzo: ${address} \nChiave privata: ${privateKey}`);
 
-var privateKey = 'cPVrtDvhfwKNq4GN3hvmm8CHhvjKViXyeERX6JeQ7UGUVGw1k4Zg';
-var ourWallet = new Btc.ECPair.fromWIF(privateKey, TestNet);
-var publicKey = ourWallet.getAddress();
-console.log("ourWallet public key:", publicKey);
+// var privateKey = 'cPVrtDvhfwKNq4GN3hvmm8CHhvjKViXyeERX6JeQ7UGUVGw1k4Zg';
+// var keyPair = new Btc.ECPair.fromWIF(privateKey, TestNet);
+// var address = keyPair.getAddress();
+// console.log(`Indirizzo: ${address} \nChiave privata: ${privateKey}`);
 
 // log balance
-request.get(apiUrl + publicKey, (err, req, body) => {
+request.get(apiUrl + address, (err, req, body) => {
     var response = JSON.parse(body);
-    console.log('Balance: ', response['balance']);
+console.log('Saldo:', response['balance']);
 });
--- create-transaction.js
+++ create-transaction.js
@@ -4,36 +4,39 @@ const TestNet = Btc.networks.testnet;
 const apiUrl = 'https://testnet.blockexplorer.com/api/addr/';
 
 var privateKey = 'cPVrtDvhfwKNq4GN3hvmm8CHhvjKViXyeERX6JeQ7UGUVGw1k4Zg';
-var ourWallet = new Btc.ECPair.fromWIF(privateKey, TestNet);
-var publicKey = ourWallet.getAddress();
-console.log("ourWallet public key:", publicKey);
+var keyPair = new Btc.ECPair.fromWIF(privateKey, TestNet);
+var address = keyPair.getAddress();
+console.log("Indirizzo:", address);
 
 // log unspent transactions
-request.get(apiUrl + publicKey + '/utxo', (err, req, body) => {
+request.get(apiUrl + address + '/utxo', (err, req, body) => {
     var response = JSON.parse(body);
-    console.log('Unspent transactions:', response);
+console.log('Lista transazioni non spese:', response);
 });
 
-var tx = new Btc.TransactionBuilder(TestNet);
+// var amountWeHave = 100000000; // 1.0 BTC
+// var amountToKeep = 90000000; // 0.9 BTC
+// var transactionFee = 1000; // 0.0001 BTC
+// var amountToSend = amountWeHave - amountToKeep - transactionFee; // ~0.1 (0.0999)
+// console.log('Importo da inviare:', amountToSend);
 
-var amountWeHave = 100000000; // 1.0 BTC
-var amountToKeep = 90000000; // 0.9 BTC
-var transactionFee = 1000; // 0.0001 BTC
-var amountToSend = amountWeHave - amountToKeep - transactionFee; // ~0.1 (0.0999)
+// var data = Buffer.from('Messaggio da salvare', 'utf8');
+// var dataScript = Btc.script.nullData.output.encode(data);
 
-console.log('Amount to send:', amountToSend);
-// tx.addInput('e4180bcd80c417173a47a98c38d8d04d0a14739040dafa074b5cd160b61bd391', 1);
+// var tx = new Btc.TransactionBuilder(TestNet);
+// tx.addInput('595231557188b872662afaea0ee55e8d2ab6fac7ee9ec938511d94d6d04cd362', 1);
 
 // // Address Bitcoin Core: mpns2b5a74asdiQP9wdx7bLmgqastegu3W
 // // Address Copay: mkrw59j2wERjWhq5NGKbnrgkvMrp1TzkWT
 
 // tx.addOutput('mpns2b5a74asdiQP9wdx7bLmgqastegu3W', amountToSend);
-// tx.addOutput(publicKey, amountToKeep);
-// tx.sign(0, ourWallet);
+// tx.addOutput(address, amountToKeep);
+// tx.addOutput(dataScript, 0)
+// tx.sign(0, keyPair);
 
 // var tx_hex = tx.build().toHex();
 
-// console.log('our beautiful transaction: ', tx_hex);
+// console.log('Transazione in esadecimale: ', tx_hex);
 
 // request.post({
 //     url:'https://testnet.blockexplorer.com/api/tx/send',
